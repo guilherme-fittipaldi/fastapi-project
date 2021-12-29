5a0ab0888fb7c334efe4015473cdc7f5eaba4586
guilherme-fittipaldi <81246770+guilherme-fittipaldi@users.noreply.github.com>
2021-12-28 21:42:25 -0300

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from googletrans import Translator
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["*"]
+origins = ["http://localhost",
+           "http://localhost:3000",
+           "http://localhost:5000",
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+           ]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 
 translator = Translator()
 

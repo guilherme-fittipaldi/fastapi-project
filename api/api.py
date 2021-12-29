5ca8aee462a2ab04064dae3052cc853b0cd29447
guilherme-fittipaldi <81246770+guilherme-fittipaldi@users.noreply.github.com>
2021-12-28 21:25:29 -0300

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from googletrans import Translator
-
-translator = Translator()
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+translator = Translator()
 
 
 @ app.get("/{title}/{ln}")
 async def hello(title, ln):
-    print(translator.translate(str(title.replace("zzz", " ")), dest=str(ln)))
     return translator.translate(str(title.replace("zzz", " ")), dest=str(ln)).text
